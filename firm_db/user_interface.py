8e9795dfdd331b227c89790cd5f9c65b8f156f6c
MaximProsh <karmark@yandex.ru>
2022-11-30 22:19:25 +0300

    fixed mistakes

--- firm_db/user_interface.py
+++ firm_db/user_interface.py
@@ -1,16 +1,21 @@
 import read_add_db
 
 def user_choise():
-    print('''Your choise:
-    1. Add new information
-    2. Delete a row
-    3. See the database''')
-    choise = int(input())
-    if choise == 1:
-        return read_add_db.add_new_line()
-    elif choise == 2:
-        return read_add_db.del_row()
-    elif choise == 3:
-        return read_add_db.read_db()
-    else:
-        print('Error input')
+    flag = 0
+    while flag != 1:
+        print('''Your choise:
+        1. Add new information
+        2. Delete a row
+        3. See the database''')
+        choise = int(input())
+        if choise == 1:
+            flag += 1
+            return read_add_db.add_new_line()
+        elif choise == 2:
+            flag += 1
+            return read_add_db.del_row()
+        elif choise == 3:
+            flag += 1
+            return read_add_db.read_db()
+        else:
+            print('Error input \n')
